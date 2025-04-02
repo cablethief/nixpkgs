a7bead7bb2a3e7f625ca8a170e8cdd6dda7b7601
cablethief <7942469+cablethief@users.noreply.github.com>
2025-04-02 19:25:04 +0100

    Create absolute secret ref

--- hosts/factorio/default.nix
+++ hosts/factorio/default.nix
@@ -6,7 +6,7 @@
 # 'let' block comes immediately after the signature
 let
   # Import host-specific secrets if using file method
-  factorioSecrets = import ./secrets/factorio-credentials.nix;
+  factorioSecrets = import /etc/nixos/hosts/factorio/secrets/factorio-credentials.nix;
 
 # 'in' is followed by ONE single attribute set containing ALL options
 in {
